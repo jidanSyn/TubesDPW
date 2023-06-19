2fa5446babb88889a9f2200733a1002c46095f26
RepanDh <133224998+RepanDh@users.noreply.github.com>
2023-06-19 17:44:36 +0700

    awal pertama

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/produk', function () {
+    return view('produk');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::get('/register', [LoginController::class, 'register'])->middleware('guest');
+Route::post('/register', [LoginController::class, 'create']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+// Route::get('/admin', function() {
+
+// });
