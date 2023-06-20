545259f006a0ab66d9e1a1e3988e45440047422d
RepanDh <133224998+RepanDh@users.noreply.github.com>
2023-06-20 08:11:16 +0700

    game

---
+++ database/migrations/2023_06_19_124514_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('game_id');
+            $table->string('name');
+            $table->string('foto');
+            $table->decimal('harga', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
