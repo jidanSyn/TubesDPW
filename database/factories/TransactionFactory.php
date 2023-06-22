700cbe68a1ac12358f6700d49b5224e7ef5951f6
Jidan <114399924+jidanSyn@users.noreply.github.com>
2023-06-22 13:37:25 +0700

    data transaksi dan produk

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TransactionFactory extends Factory
@@ -13,8 +14,24 @@ class TransactionFactory extends Factory
      */
     public function definition()
     {
-        return [
-            //
-        ];
+        
+        $dateBetween = mt_rand(1672531200, 1687412159); // 1 January 2023 - 22 June 2023
+        $date = date("Y-m-d H:i:s", $dateBetween);
+        
+        
+        $fakeData = array(     
+            'product_id' => mt_rand(1, 91),
+            'user_id' => mt_rand(3, 103),
+            'pembayaran_id' => mt_rand(1,3),
+            'purchased_on' => $date
+        );
+
+        $selectedProduct = Product::where('id', $fakeData['product_id'])->first();
+
+        $fakeData['harga'] = $selectedProduct->harga;
+
+        // dd($fakeData);
+        
+        return $fakeData;
     }
 }
